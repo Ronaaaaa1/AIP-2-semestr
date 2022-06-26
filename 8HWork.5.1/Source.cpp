427708e75c4144a8b526e25151933435f5ebf181
Вероничка Самойлова <91496169+Ronaaaaa1@users.noreply.github.com>
2022-06-27 00:28:31 +0300

    Add files via upload

---
+++ 8HWork.5.1/Source.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+using namespace std;
+int col_str, col_stb;
+
+//11. Реализовать вычисление матричных норм из списка выше (можно для квадратных матриц): 1. Преобразовать матрицу: элементы строки, в которой находится минимальный элемент матрицы, заменить нулями
+
+void operation(int** mas) {
+	int min;
+	for (int i = 0; i < col_str; i++) {
+		min = 0;
+		for (int j = 0; j < col_stb; j++) {
+			if (mas[i][j] < mas[i][min]) {
+				min = j;
+			}
+		}
+		for (int j = 0; j < col_stb; j++) {
+			if (mas[i][j] == mas[i][min]) {
+				mas[i][j] = 0;
+			}
+		}
+	}
+}
+
+int max(int* mas) {
+	int max = 0;
+	for (int i = 0; i < col_stb; i++) {
+		if (mas[i] > mas[max]) {
+			max = i;
+		}
+	}
+	return mas[max];
+}
+
+void norm1(int** mas) {
+	int* mas1 = new int[col_stb];
+
+	for (int i = 0; i < col_stb; i++) {
+		mas1[i] = 0;
+	}
+
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			if (mas[i][j] < 0) {
+				mas1[j] += abs(mas[i][j]);
+			}
+			else {
+				mas1[j] += mas[i][j];
+			}
+		}
+	}
+
+	cout << "\n\nМатричная норма 1 = " << max(mas1);
+}
+
+void norm2(int** mas) {
+	int sum = 0;
+
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			sum += pow(mas[i][j], 2);
+		}
+	}
+
+	cout << "\nМатричная норма 2 = " << sqrt(sum);
+}
+
+void norm3(int** mas) {
+	int* mas1 = new int[col_str];
+
+	for (int i = 0; i < col_str; i++) {
+		mas1[i] = 0;
+	}
+
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			if (mas[i][j] < 0) {
+				mas1[i] += abs(mas[i][j]);
+			}
+			else {
+				mas1[i] += mas[i][j];
+			}
+		}
+	}
+
+	cout << "\nМатричная норма 3 = " << max(mas1);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(static_cast<unsigned int>(time(0)));
+
+	do {
+		cout << "Введите кол-во строк: ";
+		cin >> col_str;
+		system("CLS");
+	} while (col_str < 1);
+
+	do {
+		cout << "Введите кол-во столбцов: ";
+		cin >> col_stb;
+		system("CLS");
+	} while (col_stb < 1);
+
+	int** mas = new int* [col_str];
+	int** mas1 = new int* [col_str];
+
+	for (int i = 0; i < col_str; i++) {
+		mas[i] = new int[col_stb];
+		mas1[i] = new int[col_stb];
+	}
+
+
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			if (rand() % 2 == 0) {
+				mas[i][j] = rand() % 10;
+			}
+			else {
+				mas[i][j] = -rand() % 10;
+			}
+		}
+	}
+
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			mas1[i][j] = mas[i][j];
+		}
+	}
+
+	cout << "Изначальный массив:\n";
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			cout << mas[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	operation(mas1);
+
+	cout << "\nРезультат:\n";
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			cout << mas1[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	norm1(mas);
+	norm2(mas);
+	norm3(mas);
+
+	return 0;
+}
---
+++ 8HWork.5/Source.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int col_str, col_stb;
+
+//1. Преобразовать матрицу: элементы строки, в которой находится минимальный элемент матрицы, заменить нулями.
+
+void operation(int** mas) {
+	int min;
+	for (int i = 0; i < col_str; i++) {
+		min = 0;
+		for (int j = 0; j < col_stb; j++) {
+			if (mas[i][j] < mas[i][min]) {
+				min = j;
+			}
+		}
+		for (int j = 0; j < col_stb; j++) {
+			if (mas[i][j] == mas[i][min]) {
+				mas[i][j] = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(static_cast<unsigned int>(time(0)));
+
+	do {
+		cout << "Введите кол-во строк: ";
+		cin >> col_str;
+		system("CLS");
+	} while (col_str < 1);
+
+	do {
+		cout << "Введите кол-во столбцов: ";
+		cin >> col_stb;
+		system("CLS");
+	} while (col_stb < 1);
+
+	int** mas = new int* [col_str];
+
+	for (int i = 0; i < col_str; i++) {
+		mas[i] = new int[col_stb];
+	}
+
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			if (rand() % 2 == 0) {
+				mas[i][j] = rand() % 10;
+			}
+			else {
+				mas[i][j] = -rand() % 10;
+			}
+		}
+	}
+
+	cout << "Изначальный массив:\n";
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			cout << mas[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	operation(mas);
+
+	cout << "\nРезультат:\n";
+	for (int i = 0; i < col_str; i++) {
+		for (int j = 0; j < col_stb; j++) {
+			cout << mas[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
